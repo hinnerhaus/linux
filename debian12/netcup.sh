ccf1ead50e3f98a68d7adb3ddbae3a29ebd09381
hinnerhaus <161300991+hinnerhaus@users.noreply.github.com>
2024-02-26 13:55:48 +0100

    Update netcup.sh

--- debian12/netcup.sh
+++ debian12/netcup.sh
@@ -11,12 +11,3 @@ sudo mv ./sshd_config /etc/ssh/sshd_config
 sudo chown root:root /etc/ssh/sshd_config
 rm sshd_config
 sudo service sshd restart
-
-echo "##########################################################"
-echo "#"
-echo "# Downloading init script....
-echo "#
-echo "# vvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-wget https://github.com/hinnerhaus/linux/blob/main/debian12/debian12.sh
-chmod +x debian12.sh
-./debian12.sh
