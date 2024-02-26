6ab164421c5e563e57d71542f8d1a0304805f067
hinnerhaus <161300991+hinnerhaus@users.noreply.github.com>
2024-02-26 14:00:20 +0100

    Update netcup.sh

--- debian12/netcup.sh
+++ debian12/netcup.sh
@@ -10,4 +10,8 @@ echo "Port ~ENTER_PORT_NUMBER_HERE~" >> ./sshd_config
 sudo mv ./sshd_config /etc/ssh/sshd_config
 sudo chown root:root /etc/ssh/sshd_config
 rm sshd_config
+
+sudo groupadd ssh-users
+sudo usermod -a -G ssh-users $(whoami)
+
 sudo service sshd restart
