3e519861f94b0cdcae525a9298926d4d19d9f9b4
hinnerhaus <161300991+hinnerhaus@users.noreply.github.com>
2024-02-26 12:10:39 +0100

    Create debian12.sh

---
+++ debian12.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+### Check if run as root
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+
+### Update packages
+apt update
+apt -y upgrade
+
+### Install packages
+apt -y install software-properties-common snapd
+apt -y install python3 python3-launchpadlib
+apt -y install neofetch
+
+### Install certbot
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+### Customize .bashrc
+echo "neofetch" >> ~/.bashrc
+echo "alias ll='ls -lahF --group-directories-first'" >> ~/.bashrc
+source ~/.bashrc
