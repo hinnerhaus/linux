0b43f90d6d7c93d839a21e06cf48ff540bb50442
hinnerhaus <161300991+hinnerhaus@users.noreply.github.com>
2024-03-04 09:14:14 +0100

    Update tmux.sh

--- debian12/tmux.sh
+++ debian12/tmux.sh
@@ -3,3 +3,4 @@ sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "source-file ~/.tmux/tmux.conf" > ~/.config/tmux/tmux.conf
+curl https://raw.githubusercontent.com/hinnerhaus/linux/main/debian12/tmux.conf > ~/.tmux/tmux.conf
